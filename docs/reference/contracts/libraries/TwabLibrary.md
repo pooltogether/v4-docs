1d7ad5371bb7e3effda00038b263070c0f6a4459
Brendan Asselstine <mail.asselstine@gmail.com>
2021-09-15 11:30:07 -0700

    Removed libs from docs

--- docs/reference/contracts/libraries/TwabLibrary.md
+++
@@ -1,4 +0,0 @@
-This library allows you to efficiently track a user's historic balance.  You can get a
-
-
-## Functions
--- scripts/fixContractDocs.sh
+++ scripts/fixContractDocs.sh
@@ -4,5 +4,6 @@ mv docs/reference/contracts/prize-pool/YieldSourcePrizePool.md docs/reference/co
 rm -rf docs/reference/contracts/prize-pool
 rm -rf docs/reference/contracts/external
 rm -rf docs/reference/contracts/interfaces
+rm -rf docs/reference/contracts/libraries
 rm -rf docs/reference/contracts/prize-strategy
 yarn add-contracts-category
