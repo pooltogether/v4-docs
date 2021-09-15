768a484045a72356e75b34c8c860e373a476e7bd
Brendan Asselstine <mail.asselstine@gmail.com>
2021-09-15 10:02:28 -0700

    Removed more contracts

---
+++ scripts/fixContractDocs.sh
@@ -0,0 +1,8 @@
+rm -rf docs/reference/contracts/test
+rm -rf docs/reference/contracts/external
+mv docs/reference/contracts/prize-pool/YieldSourcePrizePool.md docs/reference/contracts/YieldSourcePrizePool.md
+rm -rf docs/reference/contracts/prize-pool
+rm -rf docs/reference/contracts/external
+rm -rf docs/reference/contracts/interfaces
+rm -rf docs/reference/contracts/prize-strategy
+yarn add-contracts-category
